3be21e5cfc2a38080dfd4b6b51f0967bca3e4e54
Prithvi Suresh <psuresh2@v001.ib.bridges2.psc.edu>
2022-04-04 17:08:59 -0400

    PSC with opencv3.4 and cuda 11

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,8 @@
 #include <opencv2/core.hpp>
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgcodecs.hpp>
-#include "hello.cuh"
 #include "hello.h"
+#include "hello.cuh"
 
 using namespace cv;
 using namespace std;
@@ -13,20 +13,20 @@ int main(int argc, char** argv) {
         cout << " Usage: display_image ImageToLoadAndDisplay" << endl;
         return -1;
     }
-    Mat image;
-    image = imread(argv[1], IMREAD_COLOR);  // Read the file
-    if (image.empty())                      // Check for invalid input
-    {
-        cout << "Could not open or find the image" << std::endl;
-        return -1;
-    }
-    namedWindow("Display window",
-                WINDOW_AUTOSIZE);     // Create a window for display.
-    imshow("Display window", image);  // Show our image inside it.
-    waitKey(0);                       // Wait for a keystroke in the window
+
+  Mat image;
+  image = imread(argv[1], IMREAD_COLOR);  // Read the file
+  if (image.empty())                      // Check for invalid input
+  {
+      cout << "Could not open or find the image" << std::endl;
+      return -1;
+  }
+  namedWindow("Display window",
+              WINDOW_AUTOSIZE);     // Create a window for display.
+  imshow("Display window", image);  // Show our image inside it.
+  waitKey(0);                       // Wait for a keystroke in the window
 
     printf("Result of 1+2 is: %d\n", add(1, 2));
-
     wrap_hello(10);
     return 0;
 }
