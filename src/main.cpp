59200bb68b4f895b42c48f8cecd5de7c2d5580ca
Prithvi Suresh <prithvisuresh98@gmail.com>
2022-04-11 15:25:33 -0400

    Milestone

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,11 @@
 #include <opencv2/features2d.hpp>
 #include <hello.h>
 #include <hello.cuh>
+#include <chrono>
 
+#define NUM_ITER 200
+
+using namespace std::chrono;
 using namespace cv;
 using namespace std;
 int main(int argc, char** argv) {
@@ -21,22 +25,29 @@ int main(int argc, char** argv) {
       cout << "Could not open or find the image" << std::endl;
       return -1;
   }
-  namedWindow("Display window",
-              WINDOW_AUTOSIZE);     // Create a window for display.
-  imshow("Display window", image);  // Show our image inside it.
-  waitKey(0);                       // Wait for a keystroke in the window
-  
+ // namedWindow("Display window",
+ //             WINDOW_AUTOSIZE);     // Create a window for display.
+ // imshow("Display window", image);  // Show our image inside it.
+ // waitKey(0);                       // Wait for a keystroke in the window
+	 
   Ptr<SIFT> detector = SIFT::create();
   vector<KeyPoint> keypoints;
-  detector->detect(image, keypoints );
+  auto start = high_resolution_clock::now();
+  for(int i = 0; i < NUM_ITER; i++){ 
+    detector->detect(image, keypoints );
+  }
+  auto end = high_resolution_clock::now();
+  auto diff = duration_cast<milliseconds>(end - start);
+  cout << "Average time for " << NUM_ITER << " iterations: " << diff.count()/NUM_ITER << "ms" << endl;
+  cout << "Image Size (" << image.size().height << "," << image.size().width << ")" << endl;
     //-- Draw keypoints
-  Mat img_keypoints;
-  drawKeypoints(image, keypoints, img_keypoints );
-    //-- Show detected (drawn) keypoints
-  imshow("SIFT Keypoints", img_keypoints );
-  waitKey();
+ // Mat img_keypoints;
+ // drawKeypoints(image, keypoints, img_keypoints );
+ //   //-- Show detected (drawn) keypoints
+ // imshow("SIFT Keypoints", img_keypoints );
+ // waitKey();
 
-  printf("Result of 1+2 is: %d\n", add(1, 2));
-  wrap_hello(10);
-    return 0;
+ // printf("Result of 1+2 is: %d\n", add(1, 2));
+ // wrap_hello(10);
+  return 0;
 }
