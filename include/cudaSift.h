206d8a5b60854eea0409be00c71509cd086446f3
Prithvi Suresh <prithvisuresh98@gmail.com>
2022-04-23 12:14:02 -0400

    Integrated cudaSift with our repo

--- include/cudaSift.h
+++ include/cudaSift.h
@@ -1,26 +1,45 @@
-#ifndef _CUDASIFT_H
-#define _CUDASIFT_H
-#include <opencv2/core.hpp>
+#ifndef CUDASIFT_H
+#define CUDASIFT_H
 
-using namespace cv;
+#include "cudaImage.h"
 
-#define NUM_OCTAVES_DFL 4
-#define NUM_INTERVALS_DFL 2
+typedef struct {
+  float xpos;
+  float ypos;   
+  float scale;
+  float sharpness;
+  float edgeness;
+  float orientation;
+  float score;
+  float ambiguity;
+  int match;
+  float match_xpos;
+  float match_ypos;
+  float match_error;
+  float subsampling;
+  float empty[3];
+  float data[128];
+} SiftPoint;
 
-class cudaSift {
-private:
-  Mat image;
-  int num_octaves;
-  int num_intervals;
+typedef struct {
+  int numPts;         // Number of available Sift points
+  int maxPts;         // Number of allocated Sift points
+#ifdef MANAGEDMEM
+  SiftPoint *m_data;  // Managed data
+#else
+  SiftPoint *h_data;  // Host (CPU) data
+  SiftPoint *d_data;  // Device (GPU) data
+#endif
+} SiftData;
 
-  unsigned char ***scale_space;
+void InitCuda(int devNum = 0);
+float *AllocSiftTempMemory(int width, int height, int numOctaves, bool scaleUp = false);
+void FreeSiftTempMemory(float *memoryTmp);
+void ExtractSift(SiftData &siftData, CudaImage &img, int numOctaves, double initBlur, float thresh, float lowestScale = 0.0f, bool scaleUp = false, float *tempMemory = 0);
+void InitSiftData(SiftData &data, int num = 1024, bool host = false, bool dev = true);
+void FreeSiftData(SiftData &data);
+void PrintSiftData(SiftData &data);
+double MatchSiftData(SiftData &data1, SiftData &data2);
+double FindHomography(SiftData &data,  float *homography, int *numMatches, int numLoops = 1000, float minScore = 0.85f, float maxAmbiguity = 0.95f, float thresh = 5.0f);
 
-public:
-  cudaSift(Mat i, int no, int ni);
-  cudaSift(Mat i);
-  void init();
-  ~cudaSift();
-  void describe();
-};
-
-#endif //_CUDASIFT_H
+#endif
